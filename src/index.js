75231ed28d234a4db14d6efe51ffba65f16a221a
Born <yura.yushkevich@gmail.com>
2021-01-31 20:12:31 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+'use strict';
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (numArr) {
+    if (!numArr || numArr == 0){
+      return 0;
+  }else{
+      let minValue = numArr[0];
+      for(let i = 0; i < numArr.length; i++){
+          if (numArr[i] < minValue){
+              minValue = numArr[i];
+          }
+      }
+      return minValue;
+  }  
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (numArr) {
+  if ( !numArr || numArr == 0){
+    return 0;
+}else{
+    let maxValue = numArr[0];
+    for(let i = 0; i < numArr.length; i++){
+        if (numArr[i] > maxValue){
+            maxValue = numArr[i];
+        }
+    }
+    return maxValue;
+} 
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (numArr) {
+  if ( !numArr || numArr == 0){
+    return 0;
+}else{
+    const avgValue = numArr.reduce((sum, current)=>{
+        return sum + current;     
+    });
+    return avgValue/numArr.length;
+} 
+};
